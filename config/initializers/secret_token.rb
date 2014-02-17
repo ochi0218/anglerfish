76516a65e3b4e673677d5d6679afb52785841c83
ochi0218 <ochi0218@gmail.com>
2014-02-17 20:24:22 +0900

    開発のためのテンプレートを作成。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anglerfish::Application.config.secret_key_base = 'bd3fc88fccd95cafbeeda5a8aa6335d795d5276b47c33048f1d5bbd2333cba7ba869186cbd96a215f4f238cb04351c771b1b65bebc79e03525cff3357833888e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Anglerfish::Application.config.session_store :cookie_store, key: '_anglerfish_session'

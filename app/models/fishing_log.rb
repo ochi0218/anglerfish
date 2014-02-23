8d92baa731246ff0f65df4ae1c95a34f22e4e389
ochi0218 <ochi0218@gmail.com>
2014-02-23 17:21:54 +0900

    釣果入力時の入力検証を追加。

--- app/models/fishing_log.rb
+++ app/models/fishing_log.rb
@@ -1,2 +1,4 @@
 class FishingLog < ActiveRecord::Base
+    validates :fish_name, :fishing_date, :fishing_time, presence: true
+    validates_numericality_of :fish_length, :fish_weight, greater_than: 0
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Anglerfish::Application.routes.draw do
+  get "tides/index"
   resources :fishing_logs
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'tides#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
